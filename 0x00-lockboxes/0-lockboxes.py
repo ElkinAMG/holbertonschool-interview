f004c01de68b35966c2590dc2d9e4878b606da80
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-12-11 18:59:51 -0500

    first commit

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+def canUnlockAll(boxes):
+    """ determines if all the boxes can be opened """
+
+    keys = []
+    for i in range(len(boxes)):
+        for j in range(len(boxes[i])):
+             if i < boxes[i][j] < len(boxes):
+                 keys.append(boxes[i][j])
+        if len(boxes[i]) == 0 and i == len(boxes) - 1:
+              keys.append(0)
+    return True if len(keys) == len(boxes) else False
+
