ff293bab4abd58a82a75e335bee2a27410628334
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-12-11 19:06:11 -0500

    PEP8 Fixed

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python3
+
+
 def canUnlockAll(boxes):
     """ determines if all the boxes can be opened """
 
     keys = []
     for i in range(len(boxes)):
         for j in range(len(boxes[i])):
-             if i < boxes[i][j] < len(boxes):
-                 keys.append(boxes[i][j])
+            if i < boxes[i][j] < len(boxes):
+                keys.append(boxes[i][j])
         if len(boxes[i]) == 0 and i == len(boxes) - 1:
-              keys.append(0)
+            keys.append(0)
     return True if len(keys) == len(boxes) else False
-
