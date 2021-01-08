4d0491e413cd2430532b2aac62dfa9ef5c1a0b10
ElkinAMG <elkinandresmejiagueto@gmail.com>
2021-01-08 13:46:55 -0500

    Algorithm Project Done

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+
+
+def minOperations(n):
+    if type(n) != int or n < 0:
+        return 0
+
+    res = 0
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            res += i
+            n = n / i
+            i = i - 1
+        i = i + 1
+    return int(res)
